31d6ce7f0b4bc741e83cc0eeeb19c007e65aaafc
Chet Murthy <chetsky@gmail.com>
2020-09-19 18:37:38 -0700

    another test
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # On branch master
    # Your branch is up to date with 'origin/master'.
    #
    # Changes to be committed:
    #	modified:   tests/test_hashcons.ml
    #
    # Untracked files:
    #	tests/include_ml
    #

--- tests/test_hashcons.ml
+++ tests/test_hashcons.ml
@@ -7,3 +7,9 @@ type lam =
   | App of lam * lam
 [@@deriving hashcons { module_name = LAM }]
 ;;
+
+type variable = int (* 1..max_var *) ;;
+
+type bdd = Zero | One | Node of variable * bdd (*low*) * bdd (*high*)
+[@@deriving hashcons { module_name = BDD }]
+;;
